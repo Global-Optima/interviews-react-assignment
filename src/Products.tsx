9d6317d78b0d6e9a713cf47498d4d3b92a25fedd
osp-d <edudamirosp@gmail.com>
2025-12-06 17:50:34 +0500

    fix: cart update

--- src/Products.tsx
+++ src/Products.tsx
@@ -28,95 +28,128 @@ export type Cart = {
   items: Product[];
   totalPrice: number;
   totalItems: number;
-}
-export const Products = ({ onCartChange }: { onCartChange: (cart: Cart) => void }) => {
+};
 
+export const Products = ({
+  onCartChange,
+}: {
+  onCartChange: (cart: Cart) => void;
+}) => {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch('/products?limit=200').then(response => response.json()).then(data => setProducts(data.products));
+    fetch('/products?limit=200')
+      .then((response) => response.json())
+      .then((data) => setProducts(data.products));
   }, []);
 
   function addToCart(productId: number, quantity: number) {
-    setProducts(products.map(product => {
-      if (product.id === productId) {
-        return {
-          ...product,
-          loading: true,
-        };
-      }
-      return product;
-    }));
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            loading: true,
+          };
+        }
+        return product;
+      })
+    );
+
     fetch('/cart', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ productId, quantity }),
-    }).then(async response => {
+    }).then(async (response) => {
       if (response.ok) {
         const cart = await response.json();
-        setProducts(products.map(product => {
-          if (product.id === productId) {
-            return {
-              ...product,
-              itemInCart: (product.itemInCart || 0) + quantity,
-              loading: false,
-            };
-          }
-          return product;
-        }));
-        onCartChange(cart);
 
+        setProducts((currentProducts) =>
+          currentProducts.map((product) => {
+            if (product.id === productId) {
+              return {
+                ...product,
+                itemInCart: (product.itemInCart || 0) + quantity,
+                loading: false,
+              };
+            }
+            return product;
+          })
+        );
+
+        onCartChange(cart);
       }
     });
   }
 
   return (
-    <Box overflow="scroll" height="100%">
+    <Box overflow='scroll' height='100%'>
       <Grid container spacing={2} p={2}>
-        {products.map(product => (
+        {products.map((product) => (
           <Grid item xs={4}>
             {/* Do not remove this */}
-            <HeavyComponent/>
+            <HeavyComponent />
+
             <Card key={product.id} style={{ width: '100%' }}>
               <CardMedia
-                component="img"
-                height="150"
+                component='img'
+                height='150'
                 image={product.imageUrl}
               />
               <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
+                <Typography gutterBottom variant='h6' component='div'>
                   {product.name}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant='body2' color='text.secondary'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 </Typography>
               </CardContent>
+
               <CardActions>
-                <Typography variant="h6" component="div">
+                <Typography variant='h6' component='div'>
                   ${product.price}
                 </Typography>
-                <Box flexGrow={1}/>
-                <Box position="relative" display="flex" flexDirection="row" alignItems="center">
-                  <Box position="absolute" left={0} right={0} top={0} bottom={0} textAlign="center">
-                    {product.loading && <CircularProgress size={20}/>}
+                <Box flexGrow={1} />
+                <Box
+                  position='relative'
+                  display='flex'
+                  flexDirection='row'
+                  alignItems='center'
+                >
+                  <Box
+                    position='absolute'
+                    left={0}
+                    right={0}
+                    top={0}
+                    bottom={0}
+                    textAlign='center'
+                  >
+                    {product.loading && <CircularProgress size={20} />}
                   </Box>
-                  <IconButton disabled={product.loading} aria-label="delete" size="small"
-                              onClick={() => addToCart(product.id, -1)}>
-                    <RemoveIcon fontSize="small"/>
+                  <IconButton
+                    disabled={product.loading}
+                    aria-label='delete'
+                    size='small'
+                    onClick={() => addToCart(product.id, -1)}
+                  >
+                    <RemoveIcon fontSize='small' />
                   </IconButton>
 
-                  <Typography variant="body1" component="div" mx={1}>
+                  <Typography variant='body1' component='div' mx={1}>
                     {product.itemInCart || 0}
                   </Typography>
 
-                  <IconButton disabled={product.loading} aria-label="add" size="small"
-                              onClick={() => addToCart(product.id, 1)}>
-                    <AddIcon fontSize="small"/>
+                  <IconButton
+                    disabled={product.loading}
+                    aria-label='add'
+                    size='small'
+                    onClick={() => addToCart(product.id, 1)}
+                  >
+                    <AddIcon fontSize='small' />
                   </IconButton>
                 </Box>
-
               </CardActions>
             </Card>
           </Grid>
